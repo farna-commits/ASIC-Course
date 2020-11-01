62489d156ed47a8e74ec112a6460946b5363a1b3
Osama El Farnawany <57485975+farna-commits@users.noreply.github.com>
2020-11-01 23:35:25 +0200

    - Added register to buffer input then take last 6 bits as tail bits
    - Not tested at all yet

--- Project/Phase1/FEC_encoder.vhd
+++ Project/Phase1/FEC_encoder.vhd
@@ -16,10 +16,18 @@ entity FEC_encoder is
 end FEC_encoder;
 
 architecture FEC_encoder_arch of FEC_encoder is
+    --constant 
+    constant BUFFER_SIZE                    : integer := 12; 
     --signals 
     signal g1                               : std_logic_vector(5 downto 0); 
     signal x_output_signal, y_output_signal : std_logic; 
     signal flag                             : std_logic; 
+    signal counter                          : unsigned(6 downto 0);
+    signal counter2                         : unsigned(6 downto 0);
+    signal finished_buffering_flag          : std_logic;
+    signal finished_outputting_flag         : std_logic;
+    signal data_in_buffer                   : std_logic_vector(BUFFER_SIZE-1 downto 0);
+
     
 begin
 
@@ -28,16 +36,57 @@ begin
     y_output    <= y_output_signal; 
     data_out    <= x_output_signal when (flag = '1') else y_output_signal;
 
-    process(clk_50mhz, reset) begin 
+    --buffer input from randomizer 
+    process (clk_50mhz, reset) begin 
         if (reset = '1') then 
-            g1  <= tail_bits_in; 
+            data_in_buffer          <= (others => '0'); 
+            counter                 <= (others => '0');
+            finished_buffering_flag <= '0';
+        elsif(rising_edge(clk_50mhz)) then 
+            if(rand_out_valid = '1') then 
+                if (finished_outputting_flag = '1') then 
+                    finished_buffering_flag <= '0';
+                end if; 
+                if (counter < BUFFER_SIZE) then
+                    if (finished_buffering_flag = '0') then  
+                        data_in_buffer(to_integer(counter)) <= data_in; 
+                        counter                             <= counter + 1;
+                    end if;
+                else   
+                    finished_buffering_flag <= '1';
+                    counter                 <= (others => '0'); 
+                end if;
+            end if; 
+        end if;
+    end process; 
+
+    --start shifting after buffering finishes 
+    process(clk_50mhz, reset, counter) begin 
+        if (reset = '1') then 
+            g1                          <= (others => '0');
+            counter2                    <= (others => '0');
+            finished_outputting_flag    <= '0';
         elsif (rising_edge(clk_50mhz)) then 
-            if (rand_out_valid = '1') then 
-                g1 <= data_in & g1(5 downto 1); 
+            if (rand_out_valid = '1') then
+                if (counter = BUFFER_SIZE) then 
+                    g1 <= data_in_buffer(BUFFER_SIZE-1) & data_in_buffer(BUFFER_SIZE-2) & data_in_buffer(BUFFER_SIZE-3) & data_in_buffer(BUFFER_SIZE-4) & data_in_buffer(BUFFER_SIZE-5) & data_in_buffer(BUFFER_SIZE-6); --take tail bits as last 6 bits from buffer
+                end if;
+                if (finished_buffering_flag = '1') then 
+                    if(counter2 < BUFFER_SIZE) then 
+                        g1                          <= data_in_buffer(to_integer(counter2)) & g1(5 downto 1); 
+                        counter2                    <= counter2 + 1;
+                        finished_outputting_flag    <= '0';
+                    else
+                        counter2                    <= (others => '0'); 
+                        finished_outputting_flag    <= '1';
+
+                    end if; 
+                end if; 
             end if; 
         end if; 
     end process; 
 
+    --produce outputs 
     process (clk_100mhz, reset) begin 
         if (reset = '1') then 
             flag                    <= '1'; 
@@ -47,15 +96,15 @@ begin
         elsif(rising_edge(clk_100mhz)) then 
             if (rand_out_valid = '1') then 
                 if (flag = '1') then 
-                    x_output_signal         <= data_in xor g1(0) xor g1(3) xor g1(4) xor g1(5);
+                    x_output_signal         <= data_in_buffer(to_integer(counter2)) xor g1(0) xor g1(3) xor g1(4) xor g1(5);
                     flag                    <= '0'; 
                     FEC_encoder_out_valid   <= '1'; 
                 else 
-                    y_output_signal     <= data_in xor g1(0) xor g1(1) xor g1(3) xor g1(4);  
-                    flag                <= '1';
+                    y_output_signal         <= data_in_buffer(to_integer(counter2)) xor g1(0) xor g1(1) xor g1(3) xor g1(4);  
+                    flag                    <= '1';
                 end if; 
             end if; 
         end if;
     end process; 
 
-end FEC_encoder_arch; 
+end FEC_encoder_arch;
