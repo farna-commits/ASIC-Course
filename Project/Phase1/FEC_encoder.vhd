b8396baad1e33158c2d5af32a65857af900a7ee2
Osama El Farnawany <57485975+farna-commits@users.noreply.github.com>
2020-11-06 23:07:37 +0200

    - fec encoder fixed due to flag issue

--- Project/Phase1/FEC_encoder.vhd
+++ Project/Phase1/FEC_encoder.vhd
@@ -17,9 +17,9 @@ end FEC_encoder;
 
 architecture FEC_encoder_arch of FEC_encoder is
     --constant 
-    constant BUFFER_SIZE                      : integer := 12; 
+    constant BUFFER_SIZE                      : integer := 14; 
     --signals 
-    signal   g1                               : std_logic_vector(5 downto 0); 
+    signal   shift_reg                        : std_logic_vector(5 downto 0); 
     signal   x_output_signal, y_output_signal : std_logic; 
     signal   flag                             : std_logic; 
     signal   counter                          : unsigned(6 downto 0);
@@ -63,17 +63,17 @@ begin
     --start shifting after buffering finishes 
     process(clk_50mhz, reset, counter) begin 
         if (reset = '1') then 
-            g1                          <= (others => '0');
+            shift_reg                   <= (others => '0');
             counter2                    <= (others => '0');
             finished_outputting_flag    <= '0';
         elsif (rising_edge(clk_50mhz)) then 
             if (rand_out_valid = '1') then
                 if (counter = BUFFER_SIZE) then 
-                    g1 <= data_in_buffer(BUFFER_SIZE-1) & data_in_buffer(BUFFER_SIZE-2) & data_in_buffer(BUFFER_SIZE-3) & data_in_buffer(BUFFER_SIZE-4) & data_in_buffer(BUFFER_SIZE-5) & data_in_buffer(BUFFER_SIZE-6); --take tail bits as last 6 bits from buffer
+                    shift_reg <= data_in_buffer(BUFFER_SIZE-1) & data_in_buffer(BUFFER_SIZE-2) & data_in_buffer(BUFFER_SIZE-3) & data_in_buffer(BUFFER_SIZE-4) & data_in_buffer(BUFFER_SIZE-5) & data_in_buffer(BUFFER_SIZE-6); --take tail bits as last 6 bits from buffer
                 end if;
                 if (finished_buffering_flag = '1') then 
                     if(counter2 < BUFFER_SIZE) then 
-                        g1                          <= data_in_buffer(to_integer(counter2)) & g1(5 downto 1); 
+                        shift_reg                   <= data_in_buffer(to_integer(counter2)) & shift_reg(5 downto 1); 
                         counter2                    <= counter2 + 1;
                         finished_outputting_flag    <= '0';
                     else
@@ -94,14 +94,14 @@ begin
             y_output_signal         <= '0';
             FEC_encoder_out_valid   <= '0';
         elsif(rising_edge(clk_100mhz)) then 
-            if (rand_out_valid = '1') then 
+            if (finished_buffering_flag = '1') then 
                 if (counter2 < BUFFER_SIZE) then 
                     if (flag = '1') then 
-                        x_output_signal         <= data_in_buffer(to_integer(counter2)) xor g1(0) xor g1(3) xor g1(4) xor g1(5);
+                        x_output_signal         <= data_in_buffer(to_integer(counter2)) xor shift_reg(0) xor shift_reg(3) xor shift_reg(4) xor shift_reg(5);
                         flag                    <= '0'; 
                         FEC_encoder_out_valid   <= '1'; 
                     else 
-                        y_output_signal         <= data_in_buffer(to_integer(counter2)) xor g1(0) xor g1(1) xor g1(3) xor g1(4);  
+                        y_output_signal         <= data_in_buffer(to_integer(counter2)) xor shift_reg(0) xor shift_reg(1) xor shift_reg(3) xor shift_reg(4);  
                         flag                    <= '1';
                     end if; 
                 end if; 
