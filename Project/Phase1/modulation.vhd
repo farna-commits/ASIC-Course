e02b7f710e6120f32988ef8c7d4b5727c2de07b5
Osama El Farnawany <57485975+farna-commits@users.noreply.github.com>
2020-11-08 10:55:32 +0200

    - out_valid becomes 0 when enable is 0 - added constants for 0.707 and -0.707

--- Project/Phase1/modulation.vhd
+++ Project/Phase1/modulation.vhd
@@ -16,9 +16,14 @@ end modulation;
 architecture modulation_arch of modulation is
 
     --signals 
-    signal data_in_buffer   : std_logic;
-    signal flag             : std_logic; 
-    signal data_in_fuse     : std_logic_vector(1 downto 0); 
+    signal   data_in_buffer             : std_logic;
+    signal   flag                       : std_logic; 
+    signal   data_in_fuse               : std_logic_vector(1 downto 0); 
+    signal   done_flag                  : std_logic;
+    signal   counter_done               : integer := 0; 
+    --constants
+    constant ZeroPointSeven             : std_logic_vector(15 downto 0) := "0101101001111111";
+    constant NegativeZeroPointSeven     : std_logic_vector(15 downto 0) := "1010010110000001";
     
 begin
     data_in_fuse    <= data_in & data_in_buffer when (flag = '1') else "00"; 
@@ -26,6 +31,7 @@ begin
         if (reset = '1') then
             data_in_buffer  <= '0';
             flag            <= '0';
+            done_flag       <= '0';
         elsif (rising_edge(clk_100mhz)) then 
             if (interleaver_out_valid = '1') then 
                 data_in_buffer  <= data_in;
@@ -33,7 +39,9 @@ begin
                 if (flag = '1') then 
                     flag  <= '0';       
                 end if;
-            end if; 
+            elsif(interleaver_out_valid = '0') then 
+                done_flag   <= '1'; 
+            end if;
         end if; 
     end process;
 
@@ -47,26 +55,28 @@ begin
                 if (flag = '1') then 
                     case (data_in_fuse) is 
                         when "00" => 
-                            output1                 <= "0101101001111111"; 
-                            output2                 <= "0101101001111111"; 
+                            output1                 <= ZeroPointSeven; 
+                            output2                 <= ZeroPointSeven; 
                             modulation_out_valid    <= '1'; 
                         when "01" => 
-                            output1                 <= "1010010110000001"; 
-                            output2                 <= "0101101001111111";
+                            output1                 <= NegativeZeroPointSeven; 
+                            output2                 <= ZeroPointSeven;
                             modulation_out_valid    <= '1'; 
                         when "11" =>
-                            output1                 <= "1010010110000001"; 
-                            output2                 <= "1010010110000001";
+                            output1                 <= NegativeZeroPointSeven; 
+                            output2                 <= NegativeZeroPointSeven;
                             modulation_out_valid    <= '1'; 
                         when "10" =>
-                            output1                 <= "0101101001111111"; 
-                            output2                 <= "1010010110000001";
+                            output1                 <= ZeroPointSeven; 
+                            output2                 <= NegativeZeroPointSeven;
                             modulation_out_valid    <= '1'; 
                         when others =>
                             output1 <= (others => '0'); 
                             output2 <= (others => '0');
                     end case; 
                 end if;
+            elsif(done_flag = '1') then 
+                modulation_out_valid    <= '0';
             end if;
         end if;
     end process; 
