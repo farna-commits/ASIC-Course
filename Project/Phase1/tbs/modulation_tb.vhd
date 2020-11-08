361aac90edf1fa3bb8ecf62c7d745b1bb08138d4
Osama El Farnawany <57485975+farna-commits@users.noreply.github.com>
2020-11-08 12:25:35 +0200

    - added demodulated vector to check if as input one

--- Project/Phase1/tbs/modulation_tb.vhd
+++ Project/Phase1/tbs/modulation_tb.vhd
@@ -21,16 +21,19 @@ architecture tb_arch of modulation_tb is
     --constants 
     constant CLK_100_PERIOD                       : Time := 10 ns; 
     constant CLK_100_HALF_PERIOD                  : Time := CLK_100_PERIOD / 2;
-
+    constant ZeroPointSeven                       : std_logic_vector(15 downto 0) := "0101101001111111";
+    constant NegativeZeroPointSeven               : std_logic_vector(15 downto 0) := "1010010110000001";
     --signals 
     signal   clk_100                              : std_logic := '0'; 
     signal   reset                                : std_logic; 
     signal   en                                   : std_logic; 
     signal   test_in_vector                       : std_logic_vector(191 downto 0) := x"4B047DFA42F2A5D5F61C021A5851E9A309A24FD58086BD1E";
+    signal   demodulation_vector                       : std_logic_vector(191 downto 0) := (others => '0');
     signal   test_in_bit                          : std_logic;
     signal   test_out1_bit                        : std_logic_vector(15 downto 0) ;
     signal   test_out2_bit                        : std_logic_vector(15 downto 0) ;
     signal   out_valid                            : std_logic;
+    
     
 begin 
 
@@ -63,5 +66,32 @@ begin
         en  <= '0';
         wait;
     end process; 
+
+    --demodulation test 
+    process 
+    variable i : integer := 191;
+    begin         
+        wait until out_valid = '1'; 
+        -- wait on test_out2_bit;
+        wait for 2 ns; 
+        while (i > 0) loop 
+            if (test_out1_bit = ZeroPointSeven and test_out2_bit = ZeroPointSeven) then 
+                demodulation_vector(i)      <= '0';
+                demodulation_vector(i-1)    <= '0';
+            elsif(test_out1_bit = NegativeZeroPointSeven and test_out2_bit = NegativeZeroPointSeven) then 
+                demodulation_vector(i)      <= '1';
+                demodulation_vector(i-1)    <= '1';
+            elsif(test_out1_bit = NegativeZeroPointSeven and test_out2_bit = ZeroPointSeven) then 
+                demodulation_vector(i)      <= '1';
+                demodulation_vector(i-1)    <= '0';
+            elsif(test_out1_bit = ZeroPointSeven and test_out2_bit = NegativeZeroPointSeven) then
+                demodulation_vector(i)      <= '0';
+                demodulation_vector(i-1)    <= '1';
+            end if;
+            i := i - 2; 
+            wait for 2 * CLK_100_PERIOD;
+        end loop;
+        wait;
+    end process;
 
 end tb_arch;
